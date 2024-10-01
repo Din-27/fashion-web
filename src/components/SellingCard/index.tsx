7151afd23860434683058c1554de935842783f85
herdin27 <it06.knitto@gmail.com>
2024-10-01 16:38:07 +0700

    fix css

--- src/components/SellingCard/index.tsx
+++ src/components/SellingCard/index.tsx
@@ -5,7 +5,7 @@ export default function index(props: ProductDetailTypes.SellingCard<any[]>) {
     return (
         <div className="flex flex-col justify-center min-h-full items-center lg:space-y-4 space-y-8 py-4 px-8 lg:px-0">
             <h2 className="text-center lg:text-5xl text-4xl font-bold mb-4">{props.title}</h2>
-            <div className="lg:grid grid-cols-4 flex overflow-x-auto w-full lg:gap-x-24 gap-x-6 lg:py-3 py-4 px-8 lg:px-20">
+            <div className="lg:grid grid-cols-4 flex overflow-x-auto w-full lg:gap-24 gap-x-6 lg:py-3 py-4 px-8 lg:px-20">
                 {props.data && props.data.map((x, y: number) => (
                     <Card index={y} item={x} />
                 ))}
