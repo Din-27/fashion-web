f6ea27bb211fc7a8d33c1d3baff232ec9aa48fa2
herdin27 <it06.knitto@gmail.com>
2024-10-02 08:43:08 +0700

    add mediaQuery

--- src/components/SellingCard/index.tsx
+++ src/components/SellingCard/index.tsx
@@ -1,16 +1,21 @@
+"use client"
 import React from 'react'
 import Card from './Card'
+import useMediaQuery from '../../utility/sizeMedia/mediaQuery';
 
 export default function index(props: ProductDetailTypes.SellingCard<any[]>) {
+    const isMobileView = useMediaQuery("(min-width: 768px)")
+    const data = isMobileView ? props.data.slice(0, 4) : props.data
+    
     return (
         <div className="flex flex-col justify-center min-h-full items-center lg:space-y-4 space-y-8 py-4 px-8 lg:px-0">
             <h2 className="text-center lg:text-5xl text-4xl font-bold mb-4">{props.title}</h2>
             <div className="lg:grid grid-cols-4 flex overflow-x-auto w-full lg:gap-24 gap-x-6 lg:py-3 py-4 px-8 lg:px-20">
-                {props.data && props.data.map((x, y: number) => (
-                    <Card index={y} item={x} />
+                {data && data.map((x, y: number) => (
+                    <Card key={y} index={y} item={x} />
                 ))}
             </div>
-            <button className="border rounded-full py-2 px-14 font-semibold">{props.buttonName}</button>
+            <button className="lg:block hidden border rounded-full py-2 px-14 font-semibold">{props.buttonName}</button>
         </div>
     )
 }
---
+++ src/utility/sizeMedia/mediaQuery.ts
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
+
+function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+  useEffect(() => {
+    const matchQueryList = typeof window !== "undefined" && window.matchMedia(query);
+    function handleChange(e: { matches: boolean | ((prevState: boolean) => boolean) }) {
+      setMatches(e.matches);
+    }
+    matchQueryList.addEventListener("change", handleChange);
+    return () => {
+      matchQueryList.removeEventListener("change", handleChange);
+    };
+  }, [matches, query]);
+  return matches;
+}
+
+export default useMediaQuery;
