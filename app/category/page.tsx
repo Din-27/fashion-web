f5e773b0719e7e3037663956fae0782ae9be5fcd
herdin27 <it06.knitto@gmail.com>
2024-10-02 15:19:18 +0700

    add page category

---
+++ app/category/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { Breadcumb } from '../../src/components';
+
+export default function page() {
+  return (
+    <div className="space-y-28 px-8 py-4 lg:px-24">
+      <div className="space-y-6">
+        <Breadcumb />
+      </div>
+    </div>
+  );
+}
