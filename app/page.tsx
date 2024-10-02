7472863f8fd5f05606c56d814bec98fa9cb19a4d
herdin27 <it06.knitto@gmail.com>
2024-10-02 14:35:55 +0700

    fix UI component

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { data } from "../src/data/products";
-import useMediaQuery from "../src/utility/sizeMedia/mediaQuery";
 import { BrowseCategoryGallery, CarouselReviews, SellingCard } from "../src/components";
+import { HandleViewAllPage } from "../src/utility/ViewAllHandler";
 
-const UtilityPageArrival = () => {
-  const [viewAll, setViewAll] = useState(false);
-  const isMobileView = useMediaQuery("(max-width: 768px)");
-  const product = isMobileView ? data : viewAll ? data : data.slice(0, 4);
-  const buttonName = !viewAll ? "View All" : "Less Product";
-  const onClickViewAll = () => setViewAll((prev) => (prev ? false : true));
-
-  return { product, buttonName, onClickViewAll };
-};
-
-const UtilityPageTopSelling = () => {
-  const [viewAll, setViewAll] = useState(false);
-  const isMobileView = useMediaQuery("(max-width: 768px)");
-  const product = isMobileView ? data : viewAll ? data : data.slice(0, 4);
-  const buttonName = !viewAll ? "View All" : "Less Product";
-  const onClickViewAll = () => setViewAll((prev) => (prev ? false : true));
-
-  return { product, buttonName, onClickViewAll };
-};
+const dataCarouselReview = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
 export default function Page() {
   const {
     product: productArrival,
     buttonName: buttonNameArrival,
     onClickViewAll: onClickViewAllArrival,
-  } = UtilityPageArrival();
+  } = HandleViewAllPage(data);
   const {
     product: productTopSelling,
     buttonName: buttonNameTopSelling,
     onClickViewAll: onClickViewAllTopSelling,
-  } = UtilityPageTopSelling();
+  } = HandleViewAllPage(data);
 
   return (
     <div className="my-4 space-y-8">
@@ -55,7 +37,7 @@ export default function Page() {
         />
       </div>
       <BrowseCategoryGallery data={undefined} />
-      <CarouselReviews title={"OUR CUSTOMER HAPPY"} data={[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]} />
+      <CarouselReviews title={"OUR CUSTOMER HAPPY"} data={dataCarouselReview} />
     </div>
   );
 }
---
+++ src/utility/ViewAllHandler.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import useMediaQuery from "./sizeMedia/mediaQuery";
+
+export const HandleViewAllPage = (data: any[]) => {
+  const [viewAll, setViewAll] = useState(false);
+  const isMobileView = useMediaQuery("(max-width: 768px)");
+  const product = isMobileView ? data : viewAll ? data : data.slice(0, 4);
+  const buttonName = !viewAll ? "View All" : "Less Product";
+  const onClickViewAll = () => setViewAll((prev) => (prev ? false : true));
+  return { product, buttonName, onClickViewAll };
+};
