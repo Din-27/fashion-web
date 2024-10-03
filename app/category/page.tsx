2e1fc6c56b65cbeeef4b4159fe6befa412afc375
herdin27 <it06.knitto@gmail.com>
2024-10-03 14:01:48 +0700

    fix mobile UI

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -14,18 +14,18 @@ const FilterViewContext = () => {
 
 export default function page() {
   return (
-    <div className="min-h-screen px-2 px-8 py-4 lg:space-y-28 lg:px-24">
+    <div className="min-h-screen px-4 py-4 lg:space-y-28 lg:px-24">
       <div className="space-y-6">
         <Breadcumb />
         <div className="lg:flex lg:gap-x-8">
           <FilterWeb className="hidden lg:block" context={FilterViewContext} />
           <div className="flex-1">
             <h3 className="mb-2 text-5xl font-bold">Casual</h3>
-            <div className="mx-auto grid grid-cols-2 gap-8 lg:grid-cols-3 lg:gap-14">
+            <div className="mx-auto grid grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-14">
               {data.map((x, y) => (
-                <ProductCard key={y} index={y} item={x} height={"180px"} width={"180px"} />
+                <ProductCard key={y} index={y} item={x} height={"160px"} width={"160px"} />
               ))}
-              <div className="flex w-full justify-between lg:col-span-3">
+              <div className="mt-8 flex w-full justify-between lg:col-span-3 col-span-2">
                 <button className="flex items-center space-x-4 rounded-lg border px-4 py-2">
                   <img src={ArrowLeft.src} alt="" />
                   <span className="text-lg font-semibold">Previous</span>
