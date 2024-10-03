bda03785d5ff400d1d960f999819231e2b25b144
herdin27 <it06.knitto@gmail.com>
2024-10-03 14:08:10 +0700

    add button filter

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import ArrowLeft from "../../src/assets/arrow-left.svg";
 import ArrowRight from "../../src/assets/arrow-right.svg";
+import FilterIcon from "../../src/assets/filter-icon.svg";
 import { data } from "../../src/data/products";
 import Pagination from "../../src/components/Pagination";
 import { Breadcumb, FilterWeb, ProductCard } from "../../src/components";
@@ -20,12 +21,17 @@ export default function page() {
         <div className="lg:flex lg:gap-x-8">
           <FilterWeb className="hidden lg:block" context={FilterViewContext} />
           <div className="flex-1">
-            <h3 className="mb-2 text-5xl font-bold">Casual</h3>
+            <div className="flex justify-between">
+              <h3 className="mb-2 text-5xl font-bold">Casual</h3>
+              <button className="block rounded-full px-4 bg-gray-100 lg:hidden">
+                <img src={FilterIcon.src} alt="" />
+              </button>
+            </div>
             <div className="mx-auto grid grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-14">
               {data.map((x, y) => (
                 <ProductCard key={y} index={y} item={x} height={"160px"} width={"160px"} />
               ))}
-              <div className="mt-8 flex w-full justify-between lg:col-span-3 col-span-2">
+              <div className="col-span-2 mt-8 flex w-full justify-between lg:col-span-3">
                 <button className="flex items-center space-x-4 rounded-lg border px-4 py-2">
                   <img src={ArrowLeft.src} alt="" />
                   <span className="text-lg font-semibold">Previous</span>
