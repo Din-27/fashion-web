aa70b6c371aa127e7bc7110be9cfe96596a11e6b
herdin27 <it06.knitto@gmail.com>
2024-10-04 16:28:54 +0700

    fix UI cart page

--- src/components/Cart/OrderList.tsx
+++ src/components/Cart/OrderList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "../../assets/item.png";
 
 export default function OrderList({ dataOrder }) {
   return (
@@ -6,14 +7,12 @@ export default function OrderList({ dataOrder }) {
       <ul className="space-y-4">
         {dataOrder.map((x, y) => (
           <li key={y} className="flex items-center gap-4">
-            <img
-              src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-              alt=""
-              className="size-16 rounded object-cover"
-            />
+            <div className="rounded-xl border bg-gray-100 p-4">
+              <img src={Item.src} alt="" className="size-24 rounded object-cover" />
+            </div>
 
             <div>
-              <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
+              <h3 className="text-xl font-bold text-gray-900">Basic Tee 6-Pack</h3>
 
               <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
                 <div>
@@ -28,22 +27,7 @@ export default function OrderList({ dataOrder }) {
               </dl>
             </div>
 
-            <div className="flex flex-1 items-center justify-end gap-2">
-              <form>
-                <label htmlFor="Line1Qty" className="sr-only">
-                  {" "}
-                  Quantity{" "}
-                </label>
-
-                <input
-                  type="number"
-                  min="1"
-                  defaultValue="1"
-                  id="Line1Qty"
-                  className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                />
-              </form>
-
+            <div className="ml-auto flex flex-col items-center gap-2">
               <button className="text-gray-600 transition hover:text-red-600">
                 <span className="sr-only">Remove item</span>
 
@@ -62,6 +46,21 @@ export default function OrderList({ dataOrder }) {
                   />
                 </svg>
               </button>
+
+              <form>
+                <label htmlFor="Line1Qty" className="sr-only">
+                  {" "}
+                  Quantity{" "}
+                </label>
+
+                <input
+                  type="number"
+                  min="1"
+                  defaultValue="1"
+                  id="Line1Qty"
+                  className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+                />
+              </form>
             </div>
           </li>
         ))}
